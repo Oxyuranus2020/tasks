13ad54eceb54a4fe21324cd8347793f731cb3616
Sen Qiao <qiaosen@udel.edu>
2022-09-02 23:34:09 -0400

    Branch

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript Student Name: Sen
                 Qiao
             </header>
+            <header className="App-header">Hello World</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
